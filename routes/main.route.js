f8b83948f0b1fce9717d64c9c91984ebddc02177
Abdolmaged Alkhulaifi <nitroman2009@hotmail.com>
2018-12-30 21:00:57 +0300

    fix

--- routes/main.route.js
+++ routes/main.route.js
@@ -20,17 +20,16 @@ router.post('/testData', function (req, res) {
 });
 
 router.post('/recordMain', function (req, res) {
-    console.log(req.body)
     pool.query('WITH time1 AS ('
-    + 'INSERT INTO mainP1(time, activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
+    + 'INSERT INTO main1P(time, activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
     + 'VALUES(date_trunc(\'second\', NOW()),$1,$2,$3,$4,$5,$6,$7,$8)'
     + 'RETURNING time'
     + ')'
     + '  , time2 AS('
-    + '    INSERT INTO mainP2(time,activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
+    + '    INSERT INTO main2P(time,activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
     + 'VALUES((select time from time1),$9,$10,$11,$12,$13,$14,$15,$16)'
     + ')'
-    + 'INSERT INTO mainP3(time, activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
+    + 'INSERT INTO main3P(time, activewatt, reactivepower, apparentpower, current, voltage, cosphi, frequency, powerfactor, activeenergy)'
     + 'VALUES((select time from time1),$17,$18,$19,$20,$21,$22,$23,$24);',
     [Math.max(req.body.ActiveWatts[1],0), Math.max(req.body.ReactivePower[1],0), Math.max(req.body.ApparentPower[1],0),Math.max(req.body.Voltage[1],0),Math.max(req.body.CosPhi[1],0),Math.max(req.body.Frequency[1],0),Math.max(req.body.PowerFactor[1],0),Math.max(req.body.ActiveEnergy[1],0),
     Math.max(req.body.ActiveWatts[2],0), Math.max(req.body.ReactivePower[2],0), Math.max(req.body.ApparentPower[2],0),Math.max(req.body.Voltage[2],0),Math.max(req.body.CosPhi[2],0),Math.max(req.body.Frequency[2],0),Math.max(req.body.PowerFactor[2],0),Math.max(req.body.ActiveEnergy[2],0),
@@ -42,7 +41,6 @@ router.post('/recordMain', function (req, res) {
         });
     })
     .catch(e => setImmediate(() => {
-        throw e 
         return res.status(500).json({
         status: 'error',
     });}))
